5cfddcc45e61c5e0947b26686ca0d623a4ed8dcf
ragul <ragulthangamani2002@gmail.com>
2024-03-19 17:59:50 +0530

    Initial Commit

--- Input_Output/src/main/lab.py
+++ Input_Output/src/main/lab.py
@@ -1,39 +1,40 @@
 def get_user_input_string(prompt):
     """
     Instead of returning None this method should Get user input as a string.
-
-    Hint : use input()
+    
+    Hint: use input()
 
     :param prompt: The prompt to display to the user.
     :return: The string entered by the user.
     """
-    return None # Replace None with your code
+    return input(prompt)
 
 def get_user_input_integer(prompt):
     """
     Instead of returning None this method should Get user input as an integer.
-    Hint : use input() and  convert it into int data type using int()
+    
+    Hint: use input() and convert it into int data type using int()
+
     :param prompt: The prompt to display to the user.
     :return: The integer entered by the user.
     """
     while True:
         try:
-            return None # Replace None with your code
+            return int(input(prompt))
         except ValueError:
             print("Invalid input. Please enter an integer.")
-
 
 def get_user_input_float(prompt):
     """
     Instead of returning None this method should Get user input as a float.
-    Hint : use input() and  convert it into float  data type using float()
-
+    
+    Hint: use input() and convert it into float data type using float()
 
     :param prompt: The prompt to display to the user.
     :return: The float entered by the user.
     """
     while True:
         try:
-            return None # Replace None with your code
+            return float(input(prompt))
         except ValueError:
             print("Invalid input. Please enter a float.")
